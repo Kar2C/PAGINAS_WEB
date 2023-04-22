0d2949f6f74dac464de8653d611c96d70163323d
Kar2C <kycarrillo@uniboyaca.edu.co>
2023-04-22 12:06:24 -0500

    Aplicación 2 finalizada

--- Segunda aplicación/js/tipoTexto.js
+++ Segunda aplicación/js/tipoTexto.js
@@ -1,11 +1,11 @@
 function validar() {
     var texto = document.getElementById('texto').value;
+    var respuesta = document.getElementById('respuesta');
 
-    if (typeof texto == 'string') {
-        document.getElementById('area').value="El texto ingresado: "+texto+ " es de tipo texto";
-    } else if (typeof texto == 'number'){
-        document.getElementById('area').value="El texto ingresado: "+texto+ " es de tipo número";
+    if (isNaN(texto)) {
+        respuesta.textContent = "TEXTO";
+    } else {
+        respuesta.textContent = "NÚMERO";
     }
-    
 }
 
