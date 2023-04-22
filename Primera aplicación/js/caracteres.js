eee8b3468e09132247c74ac7d4a9cabf38625739
Kar2C <kycarrillo@uniboyaca.edu.co>
2023-04-22 11:53:57 -0500

    Aplicación 1 finalizada

--- Primera aplicación/js/caracteres.js
+++ Primera aplicación/js/caracteres.js
@@ -3,20 +3,16 @@ function validar(){
 
     if(nombre==""){
         document.getElementById('area').value="Campo en blanco";
-        document.getElementById('nombre').style.borderColor = "blue";
+        document.getElementById('nombre');
     }else if(!isNaN(nombre)){
         document.getElementById('area').value="No se pueden ingresar números";
-        document.getElementById('nombre').style.borderColor = "yellow";
-        document.getElementById('nombre').style.backgroundColor = "gray";
+        document.getElementById('nombre');
+        document.getElementById('nombre');
     }else{
         procesar(nombre);
     }
 }
 
 function procesar(nombre){
-    // var cantidad = nombre.length;
-    // var nombre_mayus = nombre.toUpperCase();
-    // var mensaje = nombre_mayus +  cantidad + ' caracteres.';
-
     document.getElementById('area').value = 'La palabra/oración ' + nombre + ' contiene ' + nombre.length + ' caracteres.';
 }
