ff4a0d51cbd59fbed2a2b4cf0e3965a3b16d5c8e
Kar2C <kycarrillo@uniboyaca.edu.co>
2023-04-22 18:03:16 -0500

    Aplicación 4 finalizada

--- Cuarta aplicación/js/nota.js
+++ Cuarta aplicación/js/nota.js
@@ -6,13 +6,11 @@ function promedio() {
     var nota3 = document.getElementById("nota3").value;
     var respuesta = document.getElementById("respuesta");
     
-    var sumaNotas = (parseFloat(nota1) +parseFloat (nota2) +parseFloat (nota3));
-    var promedio = sumaNotas/3;
+    var promedio = (parseFloat(nota1) +parseFloat (nota2) +parseFloat (nota3))/3;
 
-    if (promedio >= 3.0 ){
+    if (promedio >= 30.0 ){
         respuesta.innerHTML = "Felicitaciones "+ nombre + ",<font color=green>su nota es "+ promedio+ ",</font>"+"pasaste la materia de " +materia+".";
     } else{
         respuesta.innerHTML = "Lo siento "+ nombre + ",<font color=red>su nota es  "+ promedio+ ",</font>" + "no pasaste la materia de " +materia+".";
-
     }
 }
