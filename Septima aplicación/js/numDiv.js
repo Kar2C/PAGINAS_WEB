13818ed9a764c85fa172ed9b8eacce96173614d7
Kar2C <kycarrillo@uniboyaca.edu.co>
2023-04-22 21:13:18 -0500

    Aplicación 7 finalizada

--- Septima aplicación/js/numDiv.js
+++ Septima aplicación/js/numDiv.js
@@ -1,39 +1,35 @@
-function numDivs(){
-    var numero1= document.getElementById('numero1').value;
-    var numero2= document.getElementById('numero2').value;
-    var contador=0;
+function numDivs() {
+    var numero1 = document.getElementById('numero1').value;
+    var numero2 = document.getElementById('numero2').value;
+    var contador = 0;
 
-    if(numero1%1!=0){
-        respuesta.innerHTML= "El numero "+ numero1 + " debe ser entero";
+    if (numero1 % 1 != 0) {
+        respuesta.innerHTML = "El número " + numero1 + " no es entero";
         contador = 1;
-    } 
-    if(numero2%1!=0){
-        respuesta2.innerHTML= "El numero "+ numero2 + " debe ser entero";
+    } else if (numero1 < 0) {
+        respuesta.innerHTML = "El número " + numero1 + " debe ser positivo";
         contador = 1;
-    }
-    if (numero1<0){
-        respuesta.innerHTML= "El numero "+numero1+" debe ser positivo";
+    } else if (numero1 < 0 && numero1 % 1 != 0) {
+        respuesta.innerHTML = "El número " + numero1 + " debe ser entero positivo";
         contador = 1;
     }
-    if (numero2<0){
-        respuesta2.innerHTML= "El numero "+numero2+" debe ser positivo";
+
+    if (numero2 % 1 != 0) {
+        respuesta2.innerHTML = "El número " + numero2 + " no es entero";
+        contador = 1;
+    } else if (numero2 < 0) {
+        respuesta2.innerHTML = "El número " + numero2 + " debe ser positivo";
+        contador = 1;
+    } else if (numero2 < 0 && numero2 % 1 != 0) {
+        respuesta2.innerHTML = "El número " + numero2 + " debe ser entero positivo";
         contador = 1;
     }
-    if (numero1<0 && numero1%1!=0){
-        respuesta.innerHTML= "El numero "+numero1+" debe ser entero positivo";
-        contador = 1;
-    }
-    if(numero2<0 && numero2%1!=0){
-        respuesta2.innerHTML= "El numero "+numero2+" debe ser entero positivo";
-        contador = 1;
-    }
-    if (contador!=1){
-        if (numero1%numero2==0){
-            respuesta.innerHTML = "El numero " +numero1+ " es divisible entre el numero "+numero2;
-            respuesta2.innerHTML ="";
-        }else{
-            respuesta.innerHTML = "El numero " +numero1+ " no es divisible entre el numero "+numero2;
-            respuesta2.innerHTML = "";
+
+    if (contador != 1) {
+        if (numero1 % numero2 == 0) {
+            respuesta.innerHTML = "El número " + numero1 + " es divisible entre el numero " + numero2;
+        } else {
+            respuesta.innerHTML = "El número " + numero1 + " no es divisible entre el numero " + numero2;
         }
     }
 }
