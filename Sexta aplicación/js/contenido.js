f0d7ccb94b066880f6e1f178d1b859592ab9eb6a
Kar2C <kycarrillo@uniboyaca.edu.co>
2023-04-22 19:57:24 -0500

    Aplicación 6 finalizada

--- Sexta aplicación/js/contenido.js
+++ Sexta aplicación/js/contenido.js
@@ -1,14 +1,12 @@
 function BuscarText(texto, buscar) {
-    let regex = new RegExp(buscar, 'gi');
-    return texto.replace(regex, match => "<font color=red>"+match+"</font>");
-  }
+  var regex = new RegExp(buscar, 'gi');
+  return texto.replace(regex, match => "<font color=red>"+match+"</font>");
+}
+
 function validar(){
     var parrafo = document.getElementById("parrafo").value;
     var texto = document.getElementById("texto").value;
     var busqueda = BuscarText(parrafo, texto);
 
     document.getElementById('respuesta').innerHTML=busqueda;
-
-
-
 }
